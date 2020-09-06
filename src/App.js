181a0bb4777412fc64d77ff85efeaf5745406ad9
Harjas Preet Singh <hpsn10@gmail.com>
2020-09-06 21:35:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  
+  const timenow = new Date().getHours()
+  let greeting = '';
+  const mycss = { };
+
+  if(timenow >= 1 && timenow < 12){
+    greeting = 'Good Morning';
+    mycss.color = 'green';
+  }
+  else if(timenow >= 12 && timenow < 16){
+    greeting = 'Good Afternoon';
+    mycss.color = '#432342';
+  }
+  else if(timenow >= 16 && timenow < 19){
+    greeting = 'Good Evening';
+    mycss.color = 'orange';
+  }
+  else{
+    greeting = 'Good Night';
+    mycss.color = 'purple';
+  }
+
+
+
+  let time = new Date().toLocaleTimeString();
+  const [currTime, setTime] = useState(time);
+  
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setTime(time);
+  }
+  setInterval(updateTime, 1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello Harjas,<span style={mycss}> {greeting} </span>  <br></br>{currTime} </h1>
+    </>
   );
-}
+};
 
 export default App;
